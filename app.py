3e495917edd663e31a6ae1db846f0bea2101d9f9
sharif ahmad <sharif@rapidev.ae>
2020-02-04 10:30:17 +0500

    integrated pylint-flask to pyline

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Task {self.id}>'
+@app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
 
