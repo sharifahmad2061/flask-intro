6f3ec6bacbe9628fc9769c59046222bf182d08c5
sharif ahmad <sharif@rapidev.ae>
2020-02-04 11:13:30 +0500

    have some error in update.html

--- app.py
+++ app.py
@@ -31,5 +31,29 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "Error"
+    
+
+@app.route('/update/<int:id>')
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Error"
+    else:
+        return render_template('update.html',task=task)
+
 if __name__ == "__main__":
     app.run(debug=True)
